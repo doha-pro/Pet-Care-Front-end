d12de45d5ea4158da84d5a75a1c8d14277d3a1ba
doha-pro <doha.kassem97@gmail.com>
2022-02-21 23:10:48 +0200

    adding home contact sing in and up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeContainerComponent } from './home/home-container/home-container.component';
+import { ContactContainerComponent } from './pages/contact/contact-container/contact-container.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterationComponent } from './pages/registeration/registeration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeContainerComponent},
+  { path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'contact-us',component:ContactContainerComponent},
+  {path:'sign-up', component:RegisterationComponent},
+  { path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+/*
+
+const routes: Routes = [
+  { path: 'home', component: ProductContainerComponent },
+  { path: '', redirectTo:'/home', pathMatch :'full'},
+  { path: 'details', component: ProductDetailsComponent },
+  { path: 'add-product', component: AddProductComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registeration', component: RegisterationComponent },
+];
+*/
