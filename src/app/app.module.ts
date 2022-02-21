2191091dd73b84b8c7942d390f8fc8bed90045a1
doha-pro <doha.kassem97@gmail.com>
2022-02-21 18:43:28 +0200

    adding layout components and home page components without editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ServicesComponent } from './home/services/services.component';
+import { AboutComponent } from './home/about/about.component';
+import { OfferComponent } from './home/offer/offer.component';
+import { PosterComponent } from './home/poster/poster.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ServicesComponent,
+    AboutComponent,
+    OfferComponent,
+    PosterComponent
   ],
   imports: [
     BrowserModule,
