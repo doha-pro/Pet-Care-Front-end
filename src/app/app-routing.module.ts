e4c389d8cc07c865ec37943637231c8d778bcd42
karenayman <karenayman98@gmail.com>
2022-02-24 20:46:42 +0200

    test3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BoardingListComponent } from './boarding-list/boarding-list.component';
 
 const routes: Routes = [
-  {path:'board',component:BoardingListComponent}
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoardingListComponent } from './boarding-list/boarding-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BoardingListComponent
+    AppComponent
+    
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
